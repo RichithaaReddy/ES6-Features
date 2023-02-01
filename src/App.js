daf791a57ac5f072d735b453940edac153cc8094
richitha <richithareddie@gmail.com>
2023-02-01 21:47:49 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+//var-> function scope
+function hello(){
+  for (var i=0;i<5;i++){
+    console.log(i);
+  }
+  console.log(i); // i is also accessable here out of the loop
+  //output is 5
+  //as declared in var
+}
+hello();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//-------------------------------------------------------------------------
+
+
+//let -> block scope 
+function hy(){
+  for(let i=0;i<5;i++){
+    console.log(i);
+  }
+ // console.log(i); now it cant print
+  
 }
 
-export default App;
+hy();
+//-------------------------------------------------------------------------
+//const -> block scope and cant be reassigned
+//-------------------------------------------------------------------------
+
+//objects = collection of key value pair
+
+const person={
+  name: 'ram',
+  walk:  function() {},
+  // now in es6 it can be simply defined as
+  talk() {}
+};
+
+ person.talk()
+ person['name']="richi";  //this is hard coded
+
+ //so we take other variable
+
+const temp='name';
+person[temp.value]="bob";
+
+//-------------------------------------------------------------------------
+
+
+//this returns reference to current object
+
+const student={
+  name:"ri",
+  sub() {
+    console.log(this) //f is the refernce if that obj
+  }
+};
+ student.sub();
+
+//-------------------------------------------------------------------------
+
+// arrow functions and filters
+
+const a=[
+  {id:1, score:true},
+  {id:2, score:true},
+  {id:3, score:false},
+];
+
+const b = a.filter(function(a){return a.score;});
+const c = a.filter(a=> a.score);
+  
